ec9dbfefc19c4ee538256f2a0589301ea062ef4d
usha Ahiraw <ushaahirwarahirwar@8gamil.com>
2025-04-13 18:13:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+   
+ {/* <h2 className='head'>welcome</h2> */}
+
+ <div className='navbar'>
+  <nav>
+    <ul>
+      <li><a href="#">HOME</a></li>
+      <li><a href="#">ABOUT</a></li>
+      <li><a href="#">FEEDBACK</a></li>
+      <li><a href="#">LOGIN</a></li>
+      <li><a href="#">CONTACT</a></li>
+    </ul>
+  </nav>
+ </div>
+    </>
+  )
+}
+
+export default App
